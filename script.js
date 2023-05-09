32327242d59ecb46c98e710bf03bff61dce1b1a2
AidenistooOP <103739543+AidenistooOP@users.noreply.github.com>
2023-05-09 18:53:07 +0000

    Added a responsive grid.

---
+++ script.js
@@ -0,0 +1,13 @@
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+function submitted() {
+  alert("Thanks for signing up!")
+}
